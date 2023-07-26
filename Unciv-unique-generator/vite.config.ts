19734049f8df0d087bf08cc4a79fa4455c5e8c9b
Yair Morgenstern <yairm210@hotmail.com>
2023-07-26 17:33:53 +0300

    Add build target - /docs folder

--- Unciv-unique-generator/vite.config.ts
+++ Unciv-unique-generator/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  build: {
+    outDir: '../docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
